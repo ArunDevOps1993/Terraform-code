db4dda96ae41a921870826db3fb068b49f5d780c
ArunDevOps1993 <arunksharma1993@gmail.com>
2025-04-01 23:21:39 +0530

    main.tf file

--- main.tf
+++ main.tf
@@ -1,27 +1,35 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = "Apple"
+    # use_cli              = true                                   # Can also be set via `ARM_USE_CLI` environment variable.
+    # use_azuread_auth     = true                                   # Can also be set via `ARM_USE_AZUREAD` environment variable.
+    tenant_id            = "01508a98-a05e-4d26-a88f-c9eef9040f8a" # Can also be set via `ARM_TENANT_ID` environment variable. Azure CLI will fallback to use the connected tenant ID if not supplied.
+    storage_account_name = "30marterraformautomation"             # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "arundemo"                             # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "arunclass2"                           # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.23.0"
+      version = "4.25.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-  }
+  # Configuration options
+  features {}
   subscription_id = "0cb6bac0-cbdd-4504-8e3e-4df9e0e800af"
 }
-
-resource "azurerm_resource_group" "rg3" {
-  name     = "aruntest1"
+resource "azurerm_resource_group" "Arundemo2" {
+  name     = "arundemo2-rg"
   location = "West Europe"
 }
-
-resource "azurerm_storage_account" "stg3" {
-  name                     = "stgdemo3"
-  resource_group_name      = azurerm_resource_group.rg3.name
-  location                 = azurerm_resource_group.rg3.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+resource "azurerm_resource_group" "Arundemo3" {
+  name     = "arundemo3-rg"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "Arundemo5" {
+  name     = "arundemo6-rg"
+  location = "West Europe"
 }
